4a6c6792a090a75dae54ebb5fff4c4dd68b888ee
ljevremovic <ljevremovic@telesign.com>
2025-01-31 15:30:39 +0100

    Added getenv, adjusted path to autoload.php and fixed 2 typos

--- examples/verify_voice/2_send_custom_voice_call_with_text_to_speech.php
+++ examples/verify_voice/2_send_custom_voice_call_with_text_to_speech.php
@@ -1,14 +1,14 @@
 <?php
 
-require __DIR__ . "/../../vendor/autoload.php";
+require __DIR__ . "/../vendor/autoload.php";
 
 use telesign\enterprise\sdk\verify\VerifyClient;
 use function telesign\sdk\util\randomWithNDigits;
 
-$customer_id = "FFFFFFFF-EEEE-DDDD-1234-AB1234567890";
-$api_key = "EXAMPLE----TE8sTgg45yusumoN6BYsBVkh+yRJ5czgsnCehZaOYldPJdmFh6NeX8kunZ2zU1YWaUw/0wV6xfw==";
+$customer_id = getenv('CUSTOMER_ID') ?? 'FFFFFFFF-EEEE-DDDD-1234-AB1234567890';
+$api_key = getenv('API_KEY') ?? 'ABC12345yusumoN6BYsBVkh+yRJ5czgsnCehZaOYldPJdmFh6NeX8kunZ2zU1YWaUw/0wV6xfw==';
 
-$phone_number = "phone_number";
+$phone_number = getenv('PHONE_NUMBER') ?? '11234567890';
 $verify_code = randomWithNDigits(5);
 $tts_message = sprintf('Hello, your code is %1$s. Once again, your code is %1$s. Goodbye.',
   join(", ", str_split($verify_code)));
