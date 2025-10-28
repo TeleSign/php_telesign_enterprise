c8a76893a2f1e240e81465aba4e094804a541138
Mousumi Mohanty <mmohanty@telesign.com>
2025-10-28 06:24:30 +0000

    Pull request #23: feature/EOA-7107-Update-SS-version
    Merge in SDK/php_telesign_enterprise from feature/EOA-7107-Update-SS-version to developer
    
    Squashed commit of the following:
    
    commit 6ea058ecffaa8c12f54ca375f67185083a54fdd4
    Author: MousumiMohanty <@telesign.com>
    Date:   Mon Oct 27 12:46:24 2025 +0530
    
        Update SS version in PHP FS SDK to use the latest 4.0.1

--- examples/verify_completion/2_report_completion_after_receiving_voice_call.php
+++ examples/verify_completion/2_report_completion_after_receiving_voice_call.php
@@ -1,6 +1,6 @@
 <?php
 
-require __DIR__ . "/../vendor/autoload.php";
+require __DIR__ . "/../../vendor/autoload.php";
 
 use telesign\enterprise\sdk\verify\VerifyClient;
 use function telesign\sdk\util\randomWithNDigits;
